0ff25c2aa94c4cc612d228c567362eb9fd3f5561
FelipeFettback <wapffelipe@gmail.com>
2026-03-12 20:11:59 -0300

    Ajustes

--- backend-consultas/src/main/java/com/fiap/backend_consultas/repository/PacienteRepository.java
+++ backend-consultas/src/main/java/com/fiap/backend_consultas/repository/PacienteRepository.java
@@ -1,9 +1,5 @@
 package com.fiap.backend_consultas.repository;
-
 import com.fiap.backend_consultas.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
-
-
